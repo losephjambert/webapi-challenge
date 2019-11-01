d951cc6f6c052f2a990443bdd25bac1cf18f3db3
Joe <josephc.lambert@gmail.com>
2019-11-01 10:19:45 -0700

    moves middleware into separate directory

---
+++ middleware/project.middleware.js
@@ -0,0 +1,39 @@
+const projectModel = require('../data/helpers/projectModel.js');
+
+module.exports = { validateProject, validateProjectID };
+
+async function validateProjectID(req, res, next) {
+  const { id } = req.params;
+  try {
+    const project = await projectModel.get(id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      res.status(404).send({ message: `Project with id ${id} does not exist` });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: `Error retrieving project with id ${id} from database`
+    });
+  }
+}
+
+function validateProject(req, res, next) {
+  if (!req.body) {
+    res.status(400).send({ message: `Missing project data` });
+  } else {
+    const { name, description, completed } = req.body;
+    if (!name) {
+      res.status(400).send({ message: `Project missing name field` });
+    } else if (!description) {
+      res.status(400).send({ message: `Project missing description field` });
+    } else if (typeof completed === 'undefined') {
+      res.status(400).send({ message: `Project missing completed field` });
+    } else {
+      req.project = { name, description, completed };
+      next();
+    }
+  }
+}
--- routes/projects.router.js
+++ routes/projects.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const projectModel = require('../data/helpers/projectModel.js');
+const projectMiddleware = require('../middleware/project.middleware.js');
 
 const router = express.Router();
 router.use(express.json());
+
+const { validateProject, validateProjectID } = projectMiddleware;
 
 router.get('/', async (req, res) => {
   try {
@@ -59,41 +62,5 @@ router.delete('/:id', validateProjectID, async (req, res) => {
       .send({ message: `Error deleting project ${id} from the database` });
   }
 });
-
-async function validateProjectID(req, res, next) {
-  const { id } = req.params;
-  try {
-    const project = await projectModel.get(id);
-    if (project) {
-      req.project = project;
-      next();
-    } else {
-      res.status(404).send({ message: `Project with id ${id} does not exist` });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      message: `Error retrieving project with id ${id} from database`
-    });
-  }
-}
-
-function validateProject(req, res, next) {
-  if (!req.body) {
-    res.status(400).send({ message: `Missing project data` });
-  } else {
-    const { name, description, completed } = req.body;
-    if (!name) {
-      res.status(400).send({ message: `Project missing name field` });
-    } else if (!description) {
-      res.status(400).send({ message: `Project missing description field` });
-    } else if (typeof completed === 'undefined') {
-      res.status(400).send({ message: `Project missing completed field` });
-    } else {
-      req.project = { name, description, completed };
-      next();
-    }
-  }
-}
 
 module.exports = router;
