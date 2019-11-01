fbaed0eeb1aca48c1d62404a1b0985747b9437b6
Joe <josephc.lambert@gmail.com>
2019-11-01 09:00:48 -0700

    creates projects router

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const helmet = require('helmet')
 
+// this is where I'll import my routers
+const projectsRouter = require('./routes/projects.router.js')
+
 const app = express()
 app.use(helmet())
+
+// this is where I'll set up my routers
+app.use('/api/projects', projectsRouter)
 
 app.get('/', (req, res) => {
   res.send(`
---
+++ routes/projects.router.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+  res.send({
+    message: `Hello from the projects router`,
+    requestMethod: req.method,
+    originalUrl: req.originalUrl
+  })
+})
+
+module.exports = router
