847ad3fb5a92d0c2224664b60918583cb823695f
Joe <josephc.lambert@gmail.com>
2019-11-01 09:15:31 -0700

    feature: GET projects

--- routes/projects.router.js
+++ routes/projects.router.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
+const projectModel = require('../data/helpers/projectModel.js');
 
-router.get('/', (req, res) => {
-  res.send({
-    message: `Hello from the projects router`,
-    requestMethod: req.method,
-    originalUrl: req.originalUrl
-  });
+router.get('/', async (req, res) => {
+  try {
+    const projects = await projectModel.get();
+    res.status(200).send(projects);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: `There was an error retrieving projects from the database`
+    });
+  }
 });
 
 module.exports = router;
