66988fd3c01a5e7bfbaeb6d71a2526e88422ec16
Joe <josephc.lambert@gmail.com>
2019-11-01 11:00:23 -0700

    updates env PORT variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const PORT = process.env.port || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`App is now listening at port ${PORT}`);
