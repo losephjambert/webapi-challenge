82e7d5e27294fcc2e973e1c0bcfcffbd99277078
Joe <josephc.lambert@gmail.com>
2019-11-01 10:25:04 -0700

    feature: GET all actions

--- routes/actions.router.js
+++ routes/actions.router.js
@@ -1,11 +1,26 @@
-const router = require('express').Router();
+const express = require('express');
+const actionModel = require('../data/helpers/actionModel.js');
+const projectMiddleware = require('../middleware/project.middleware.js');
 
-router.get('/', (req, res) => {
-  res.send({
-    message: `Hello from the actions router`,
-    requestMethod: req.method,
-    originalUrl: req.originalUrl
-  });
+const router = express.Router();
+router.use(express.json());
+
+const { validateProject, validateProjectID } = projectMiddleware;
+
+router.get('/', async (req, res) => {
+  try {
+    const actions = await actionModel.get();
+    res.status(200).send(actions);
+  } catch (error) {
+    const logString = `${req.method} ${req.originalUrl}`;
+    console.log(`${logString} error`, error);
+    res
+      .status(500)
+      .send({
+        request: `${logString}`,
+        message: `Error retrieving actions from database`
+      });
+  }
 });
 
 module.exports = router;
